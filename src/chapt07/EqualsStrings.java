5cb5214a8fdc44f52c32f2ed46d4808fed229759
esustreba <strebkov@td-esat.com.ua>
2018-04-04 23:12:34 +0300

    Chapter #7

---
+++ src/chapt07/EqualsStrings.java
@@ -0,0 +1,16 @@
+package chapt07;
+
+public class EqualsStrings {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        String s3 = new String("Java");
+        System.out.println(s1 + " == " + s2 + " : " + (s1 == s2));
+        System.out.println(s1 + " == " + s3 + " : " + (s1 == s3));
+        System.out.println(s1 + " equals " + s2 + " : " + (s1.equals(s2)));
+        System.out.println(s1 + " equals " + s2 + " : " + (s1.equals(s3)));
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
+    }
+}
