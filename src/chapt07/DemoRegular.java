1627fd51b00be590feaf269b87e276ae58c3e05a
esustreba <strebkov@td-esat.com.ua>
2018-04-05 13:49:51 +0300

    Chapter #7

---
+++ src/chapt07/DemoRegular.java
@@ -0,0 +1,25 @@
+package chapt07;
+
+import java.util.regex.*;
+
+public class DemoRegular {
+    public static void main(String[] args) {
+        Pattern p1 = Pattern.compile("a+y");
+        Matcher m1 = p1.matcher("aaay");
+        boolean b = m1.matches();
+        System.out.println(b);
+
+        String regex = "(\\w+)@(\\w+\\.)(\\w+)(\\.\\w+)*";
+        String s = "e-mail address: dmytrostrebkov@gmail.com and strebkov@tdesatcom.ua";
+
+        Pattern p2 = Pattern.compile(regex);
+        Matcher m2 = p2.matcher(s);
+        while (m2.find())
+            System.out.println("e-mail: " + m2.group());
+
+        Pattern p3 = Pattern.compile("\\d+\\s?");
+        String[] words = p3.split("java5tiger 77 java6mustang");
+        for(String word : words)
+            System.out.println(word);
+    }
+}
---
+++ src/chapt07/Groups.java
@@ -0,0 +1,25 @@
+package chapt07;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Groups {
+    public static void main(String[] args) {
+        String input = "abdcxyz";
+        myMathces("([a-z]*)([a-z]+)", input);
+        myMathces("([a-z]?)([a-z]+)", input);
+        myMathces("([a-z]+)([a-z]*)", input);
+        myMathces("([a-z]?)([a-z]?)", input);
+
+    }
+
+    public static void myMathces(String regex, String input) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.matches()) {
+            System.out.println("First group: " + matcher.group(1));
+            System.out.println("Second group: " + matcher.group(2));
+        }   else System.out.println("Nothing");
+        System.out.println();
+    }
+}
